aad0d15d9ccd66204567c40c168d7f233aea4adf
Karthik Rangarajan <karthik@addepar.com>
2014-08-21 19:24:27 -0700

    Add option to email via gmail or other authenticated service.

--- core/notifier.py
+++ core/notifier.py
@@ -4,20 +4,34 @@ from email.mime.text import MIMEText
 import datetime
 import smtplib
 
+class EmailNotifierException(Exception):
+    def __init__(self, error):
+        self.error = error
+
+    def __str__(self):
+        return repr(self.error)
 
 class EmailNotifier:
-    def __init__(self, email_from, email_to, subject, text, connection_string='localhost'):
+    def __init__(self, email_from, email_to, subject, text, connection_string='localhost', login_to_smtp=False,
+                 smtp_username = None, smtp_password = None, use_tls = None):
         self.email_from = email_from
         self.email_to = email_to
         self.connection_string = connection_string
         self.mime_message = MIMEMultipart()
         self.prepare_message_headers(subject)
         self.create_mime_message(text)
+        self.login_to_smtp = login_to_smtp
+        if self.login_to_smtp:
+            self.username = smtp_username
+            self.password = smtp_password
+            self.use_tls = use_tls
 
     @staticmethod
-    def create_notification(from_addr, to_addr, text, connection_string='localhost'):
+    def create_notification(from_addr, to_addr, text, connection_string='localhost', login_to_smtp=False,
+                            smtp_username = None, smtp_password = None, use_tls = None):
         subject = "[repoguard] possibly vulnerable changes - %s" % datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
-        return EmailNotifier(from_addr, to_addr, subject, text, connection_string)
+        return EmailNotifier(from_addr, to_addr, subject, text, connection_string, login_to_smtp,
+                             smtp_username, smtp_password, use_tls)
 
     def prepare_message_headers(self, subject):
         self.subject = subject
@@ -33,9 +47,18 @@ class EmailNotifier:
         if self.email_from and self.email_to and self.mime_message:
             self.smtp_send()
         else:
-            raise Exception("Mails should have FROM, TO headers and a message as well!")
+            raise EmailNotifierException("Mails should have FROM, TO headers and a message as well!")
 
     def smtp_send(self):
-        smtp = smtplib.SMTP(self.connection_string)
-        smtp.sendmail(self.email_from, self.email_to, self.mime_message.as_string())
-        smtp.quit()
+        try:
+            smtp = smtplib.SMTP(self.connection_string)
+            if self.login_to_smtp:
+                if self.use_tls:
+                    smtp.starttls()
+                smtp.login(self.username, self.password)
+            smtp.sendmail(self.email_from, self.email_to, self.mime_message.as_string())
+            smtp.quit()
+        except smtplib.SMTPException, e:
+            raise EmailNotifierException(e)
+
+
