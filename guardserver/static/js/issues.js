eeb0a2fb596b9a29299bf055161037b3d66a6949
Christina Nguyen <christina.nguyen@addepar.com>
2014-09-24 16:18:12 -0700

    random css

--- guardserver/static/js/issues.js
+++ guardserver/static/js/issues.js
@@ -31,7 +31,7 @@ function add_issues_to_table(data, dom_element) {
     server = localStorage.getItem("server");
     $.each(data, function(){
         var source = this["_source"];
-        var status_change = source["false_positive"] == "true" ? ["Valid", false]: ["Invalid", true];
+        var status_change = source["false_positive"] == "true" ? ["<span class='glyphicon glyphicon-flag'></span><br>Valid", false]: ["<span class='glyphicon glyphicon-ban-circle'></span><br>False", true];
         var table_row = '<tr data-repo="' + source["repo_name"] + '">' +
             "<td>" + source["repo_name"] + "</td>" +
             "<td>" + $("<div />").text(source["matching_line"]).html() + "</td>" +
@@ -39,8 +39,8 @@ function add_issues_to_table(data, dom_element) {
             "<td>" + source["commit_description"] + "</td>" +
             "<td title='" + source["description"] + "'>" + source["check_id"] + "</td>" +
             "<td class='reviewer'>" + source["last_reviewer"] +
-            '<br><button type="button" class="btn btn-status btn-primary" id="' + this["id"] + '" data-status="' + status_change[1] + '">' +
-            'Mark As ' + status_change[0] + '</button>' +
+            '<button type="button" class="btn btn-primary btn-status" id="' + this["id"] + '" data-status="' + status_change[1] + '">' +
+            status_change[0] + '</button>' +
             "</tr>";
         $(dom_element).prepend(table_row);
         $("#" + source["commit_id"]).click(function(){
